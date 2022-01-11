2df36af85b0467de69f05619abec3fb7096680af
Fergus McClean <fergus.mcclean@newcastle.ac.uk>
2022-01-11 16:50:34 +0000

    Split FILE_PATHS

--- script.py
+++ script.py
@@ -5,12 +5,12 @@ data_path = os.getenv('DATA_PATH', '/data')
 outputs_path = os.path.join(data_path, 'outputs')
 if not os.path.exists(outputs_path):
     os.mkdir(outputs_path)
-paths = os.getenv('FILE_PATHS', ['file.txt'])
+paths = os.getenv('FILE_PATHS', 'file.txt')
 folder = os.path.join(outputs_path, os.getenv('FOLDER_NAME', 'folder'))
 
 if not os.path.exists(folder):
     os.mkdir(folder)
 
-for path in paths:
+for path in paths.split(','):
     dest = os.path.join(folder, os.path.basename(path))
     shutil.move(os.path.join(data_path, 'inputs', path), dest)
