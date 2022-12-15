3ce087305e540139753e8d3ca811e9bf60cc1d09
OliviaButters <111284147+OliviaButters@users.noreply.github.com>
2022-12-15 11:57:03 +0000

    Update script.py

--- script.py
+++ script.py
@@ -2,6 +2,12 @@ import os
 import shutil
 import glob
 
+import random
+import string
+import logging
+from pathlib import Path
+from os.path import isfile, join, isdir
+
 data_path = os.getenv('DATA_PATH', '/data')
 outputs_path = os.path.join(data_path, 'outputs')
 if not os.path.exists(outputs_path):
@@ -9,10 +15,32 @@ if not os.path.exists(outputs_path):
 paths = os.getenv('FILE_PATHS', 'file.txt')
 folder = os.path.join(outputs_path, os.getenv('FOLDER_NAME', 'folder'))
 
+# Set up log file
+logger = logging.getLogger('move-files')
+logger.setLevel(logging.INFO)
+log_file_name = 'move-files-%s.log' %(''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6)))
+fh = logging.FileHandler( Path(join(data_path, outputs_path)) / log_file_name)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+
+logger.info('Log file established!')
+logger.info('--------')
+
+logger.info('Paths have been setup')  
+
+print(paths)
+logger.info(paths)
+print(folder)
+logger.info(folder)
+
+
 if not os.path.exists(folder):
     os.mkdir(folder)
 
 for path in paths.split(','):
     files = glob.glob(data_path + '/**/' + path, recursive = True)
+    print(files)
+    logger.info(files)
     dest = os.path.join(folder, os.path.basename(files[0]))
     shutil.move(os.path.join(data_path, 'inputs',files[0]), dest)
