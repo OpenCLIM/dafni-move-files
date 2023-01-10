6bd01732323408b017bf8874282a59c98bd5077c
OliviaButters <111284147+OliviaButters@users.noreply.github.com>
2023-01-10 12:02:12 +0000

    Update script.py

--- script.py
+++ script.py
@@ -6,7 +6,8 @@ from glob import glob
 import random
 import string
 import logging
-from pathlib import Path
+#from pathlib import Path
+from os import listdir, getenv, mkdir, remove, walk
 from os.path import isfile, join, isdir
 
 data_path = os.getenv('DATA_PATH', '/data')
